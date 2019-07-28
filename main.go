345cebe722c45611622e99173e908fa0a5630d2a
Greg Brown <grgbrn@gmail.com>
2019-07-28 17:47:48 +0200

    wander output to a timestamped logfile

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"flag"
 	"fmt"
@@ -92,7 +93,7 @@ func (net *SeenNetwork) Describe() string {
 	return fmt.Sprintf("%s [%s] %d", net.SSID, net.BSSID, net.Frequency)
 }
 
-func wanderLoop(interfaceName string) {
+func wanderLoop(interfaceName string, log *bufio.Writer) {
 	scanner := wifi.ScanManager
 	scanner.NetInterface = interfaceName
 
@@ -101,11 +102,13 @@ func wanderLoop(interfaceName string) {
 	for { // ever
 		time.Sleep(time.Second * 10)
 		now := time.Now()
+
 		fmt.Printf(">>> starting scan - %v\n", now)
+		fmt.Fprintf(log, ">>> starting scan - %v\n", now)
 
 		bssList, err := scanner.Scan()
 		if err != nil {
-			fmt.Printf("Error scanning network:%v\n", err)
+			fmt.Fprintf(log, "Error scanning network:%v\n", err)
 			continue
 		}
 
@@ -138,10 +141,10 @@ func wanderLoop(interfaceName string) {
 					AgeHistory:    []uint32{bss.Age},
 				}
 				networks[bssid] = net
-				fmt.Printf("    new network:%s\n", net.Describe())
+				fmt.Fprintf(log, "    new network:%s\n", net.Describe())
 				newCount++
 			} else { // known network, just update signal
-				fmt.Printf("    updating network:%s\n", net.Describe())
+				fmt.Fprintf(log, "    updating network:%s\n", net.Describe())
 				net.SignalHistory = append(net.SignalHistory, bss.Signal)
 				net.AgeHistory = append(net.AgeHistory, bss.Age)
 				updateCount++
@@ -155,15 +158,15 @@ func wanderLoop(interfaceName string) {
 			if !ok {
 				// set the final seen time and remove the network
 				// from the map
-				fmt.Printf("    removing network:%s\n", net.Describe())
+				fmt.Fprintf(log, "    removing network:%s\n", net.Describe())
 				net.Last = now
 
 				// XXX log this into our master network history
 				b, err := json.MarshalIndent(net, "", "  ")
 				if err != nil {
-					fmt.Println("can't marshal data:", err)
+					fmt.Fprintln(log, "can't marshal data:", err)
 				}
-				fmt.Println(string(b))
+				fmt.Fprintln(log, string(b))
 
 				delete(networks, _bss)
 				delCount++
@@ -177,9 +180,18 @@ func wanderLoop(interfaceName string) {
 		// }
 		// fmt.Print(string(b))
 
-		fmt.Printf(">>> currently %d networks in range\n", len(networks))
-		fmt.Printf(">>> %d updated / %d new / %d removed\n", updateCount, newCount, delCount)
 		fmt.Printf(">>> completed scan - %v\n", time.Now())
+
+		fmt.Fprintf(log, ">>> currently %d networks in range\n", len(networks))
+		fmt.Fprintf(log, ">>> %d updated / %d new / %d removed\n", updateCount, newCount, delCount)
+		fmt.Fprintf(log, ">>> completed scan - %v\n", time.Now())
+
+		err = log.Flush()
+		if err != nil {
+			fmt.Println("Error flushing log")
+			fmt.Println(err)
+		}
+
 	}
 }
 
@@ -228,18 +240,27 @@ func main() {
 		err = statusExample(interfaceName)
 	case "connect":
 		connectCmd.Parse(os.Args[2:])
-		fmt.Printf(">>> connect to net:'%s' on interface:%s\n", connectNet, interfaceName)
+		fmt.Printf(">>> connect to net:'%s' on interface:%s\n", *connectNet, interfaceName)
 		err = connectExample(interfaceName, *connectNet, *connectPass)
 	case "disconnect":
 		disconnectCmd.Parse(os.Args[2:])
-		fmt.Printf(">>> disconnecting from net:'%s' on interface:%s\n", connectNet, interfaceName)
+		fmt.Printf(">>> disconnecting from net:'%s' on interface:%s\n", *connectNet, interfaceName)
 		err = disconnectExample(interfaceName, *disconnectNet)
 	case "scan":
 		fmt.Printf(">>> single scan of network:%s\n", interfaceName)
 		err = scanExample(interfaceName)
 	case "wander":
 		fmt.Printf(">>> start monitoring network:%s\n", interfaceName)
-		wanderLoop(interfaceName)
+
+		// create a per-session log for this
+		// just log to pwd for now XXX
+		logName := time.Now().Format("2006-01-02_15:04:05.log")
+		fmt.Printf(">>> logging session to %s\n", logName)
+		f, err := os.Create(logName)
+		if err == nil {
+			defer f.Close()
+			wanderLoop(interfaceName, bufio.NewWriter(f))
+		}
 	default:
 		fmt.Printf("Expected subcommand: %s\n", strings.Join(validCommands, ","))
 	}
